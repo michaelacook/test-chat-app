122a4823c1dd8c0ebd3e90302fc4250acf37c3aa
Michael A. Cook <mcook0775@gmail.com>
2021-05-12 20:11:05 -0400

    Remove segment

--- src/App.js
+++ src/App.js
@@ -28,45 +28,38 @@ function App() {
 
   return (
     <Container>
-      <Segment
-        style={{
-          marginTop: "10px",
-          height: "98vh",
-        }}
-      >
-        <Grid style={{ height: "100%" }} columns={1}>
-          <Grid.Column style={{ overflow: "auto" }}>
-            {received.length
-              ? received.map((msg) => {
-                  return (
-                    <React.Fragment>
-                      <Message size="large" compact>
-                        {msg}
-                      </Message>
-                      <br></br>
-                    </React.Fragment>
-                  )
-                })
-              : null}
-          </Grid.Column>
-          <Grid.Column verticalAlign="bottom">
-            <Input
-              value={message}
-              onChange={(e) => {
-                setMessage(e.target.value)
-              }}
-              fluid
-              size="big"
-              placeholder="Send Message..."
-              action={
-                <Button color="blue" onClick={sendMessage}>
-                  Send
-                </Button>
-              }
-            ></Input>
-          </Grid.Column>
-        </Grid>
-      </Segment>
+      <Grid style={{ height: "100vh" }} columns={1}>
+        <Grid.Column style={{ overflow: "auto" }}>
+          {received.length
+            ? received.map((msg) => {
+                return (
+                  <React.Fragment>
+                    <Message size="large" compact>
+                      {msg}
+                    </Message>
+                    <br></br>
+                  </React.Fragment>
+                )
+              })
+            : null}
+        </Grid.Column>
+        <Grid.Column verticalAlign="bottom">
+          <Input
+            value={message}
+            onChange={(e) => {
+              setMessage(e.target.value)
+            }}
+            fluid
+            size="big"
+            placeholder="Send Message..."
+            action={
+              <Button color="blue" onClick={sendMessage}>
+                Send
+              </Button>
+            }
+          ></Input>
+        </Grid.Column>
+      </Grid>
     </Container>
   )
 }
